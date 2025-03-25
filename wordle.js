9e65272c05d1e35490254edb6d2c66735210e009
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-03-25 14:29:55 +0100

    function and tests for algorithm A

--- wordle.js
+++ wordle.js
@@ -1,3 +1,31 @@
-export default function findLetters() {
+export default function findLetters(guess, answer) {
     
+    const guessedWord = guess.toUpperCase().split(''); 
+    const answerWord = answer.toUpperCase().split(''); 
+
+    const result = [];
+
+    
+    guessedWord.forEach((letter, index) => {
+        if (letter === answerWord[index]) {
+            result.push({ letter, result: 'correct' });
+        } else {
+            result.push({ letter, result: 'incorrect' });
+        }
+    });
+
+    guessedWord.forEach((letter, index) => {
+        if (result[index].result === 'incorrect') {
+           
+            const letterInAnswer = answerWord.filter(l => l === letter).length;
+            const letterInResult = result.filter(r => r.letter === letter && r.result === 'correct').length;
+
+            if (letterInAnswer > letterInResult) {
+                result[index].result = 'misplaced';
+            }
+        }
+    });
+
+
+ return result;
 }
--- wordle.test.js
+++ wordle.test.js
@@ -1,7 +1,49 @@
 import findLetters from "./wordle";
 import { describe, expect, it } from '@jest/globals';
 
-/*
 
+// för att köra ett första, enkelt test:
+describe('findLetters()', () => {
+    it('has two parameters', () => {
+        expect(findLetters.length).toBe(2);
+    })
 
-*/
+// Först ut ska det funka att få correct som svar
+    it('should return correct when the letters match at the same index', () => {
+        const output = findLetters('cykla', 'cykla');
+        expect(output).toEqual([
+          { letter: 'C', result: 'correct' },
+          { letter: 'Y', result: 'correct' },
+          { letter: 'K', result: 'correct' },
+          { letter: 'L', result: 'correct' },
+          { letter: 'A', result: 'correct' }
+        ]);
+      });
+
+      // om det finns en bokstav som finns i ordet men är på fel plats så ska den få misplaced som resultat
+      it('should return misplaced if a letter exists in the word but in wrong place', () => {
+        const output = findLetters('hallå', 'cykla');
+        expect(output).toEqual([
+          { letter: 'H', result: 'incorrect' },
+          { letter: 'A', result: 'misplaced' },
+          { letter: 'L', result: 'incorrect' },
+          { letter: 'L', result: 'correct' },
+          { letter: 'Å', result: 'incorrect' }
+        ]);
+      });
+
+      //om det finns fler än en av samma bokstav och en redan är på rätt plats så ska alla andra bli incorrect
+      it('returns incorrect if there is multiple of the same letter', () => {
+        const output = findLetters('banana', 'taru');
+        expect(output).toEqual([
+          { letter: 'B', result: 'incorrect' },
+          { letter: 'A', result: 'correct' },
+          { letter: 'N', result: 'incorrect' },
+          { letter: 'A', result: 'incorrect' },
+          { letter: 'N', result: 'incorrect' },
+          { letter: 'A', result: 'incorrect' }
+        ]);
+
+        console.log(output);
+      });
+    });
