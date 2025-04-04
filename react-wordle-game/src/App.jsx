2292d499138a37e5eab914ab0767cd76822e1452
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-04-04 11:19:30 +0200

    Learning from scratch

--- react-wordle-game/src/App.jsx
+++ react-wordle-game/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-
-
   return (
-    <h1>Wordle game</h1>
-  )
-  
+    <form className="form">
+      <h1>WORDLE GAME</h1>
 
+      <div className='quantity-section'>
+      <label for="quantity">Number of letters:</label>
+      <select id="quantity" name="quantity">
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+      </select>
+      </div>
+
+      <div className="letter-input">
+        <input className="letter-input-field" maxlength="1"></input>
+        <input className="letter-input-field" maxlength="1"></input>
+        <input className="letter-input-field" maxlength="1"></input>
+        <input className="letter-input-field" maxlength="1"></input>
+        <input className="letter-input-field" maxlength="1"></input>
+        <input className="letter-input-field" maxlength="1"></input>
+      </div>
+
+      <input className="word-input"></input>
+    </form>
+  );
 }
 
-export default App
+export default App;
